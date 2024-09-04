2ab0b5e7dcb64f82460adb63c4438828971982f9
Romina-Castillo <rominacastillo054@gmail.com>
2024-09-03 21:19:38 -0300

    ...

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,14 @@
-import { AppBar, Button, Drawer, IconButton, Toolbar, Typography } from "@mui/material"
+import { AppBar, Button, Drawer, IconButton, Toolbar, Typography, MenuIcon} from "@mui/material"
 import NavListDrawer from "./NavListDrawer"
 import { useState } from "react";
+
+const navLinks = [
+    {   
+        title: "Home", path: "#"
+    },
+]
+
+
 
 export default function Navbar() {
 
@@ -15,16 +23,26 @@ export default function Navbar() {
                     size="large"
                     onClick={() => setOpen(true)}
                     >
-                    
+                     <MenuIcon />
                 </IconButton>
-                <Typography 
+                <Typography  
                 variant="h6" 
                 sx={{ flexGrow: 1 }}
                 > UVAPP 
                 </Typography>
+
                 
+
+
+
+                {
+                    navLinks.map(item=> (
+                        <Button color="inherit" key={item.title}>{item.title} </Button>
+                    ))
+                }
                 <Button color="inherit"> Home </Button>
                 <Button color="inherit"> Login </Button>
+                <Button color="inherit"> Registrarse </Button>
                 </Toolbar>
             </AppBar>
                 <Drawer
