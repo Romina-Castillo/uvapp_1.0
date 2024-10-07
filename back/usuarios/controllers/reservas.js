a390c239a006b41cea9a8629b37a1a0a8b79d52a
Romina-Castillo <rominacastillo054@gmail.com>
2024-10-07 16:12:09 -0300

    revision de cambios

--- back/usuarios/controllers/reservas.js
+++ back/usuarios/controllers/reservas.js
@@ -13,21 +13,20 @@ exports.crearReserva = (req, res) => {
             res.status(200).send('Reserva realizada exitosamente');
         }
     });
-    }
+}
 
 // Controlador para obtener reservas por id_usuario
 exports.obtenerReservasPorUsuario = (req, res) => {
     const { id } = req.params; // El ID del usuario desde los parámetros de la ruta
-  
+
     const sql = 'SELECT * FROM reservas WHERE id_usuario = ?'; // Filtrar por id_usuario
     db.query(sql, [id], (err, result) => {
-      if (err) {
-        console.error('Error al obtener las reservas:', err);
-        res.status(500).json({ message: 'Error al obtener las reservas' });
-      } else {
-        console.log("Reservas encontradas:", result); // Verifica qué datos está devolviendo la base de datos
-        res.status(200).json(result); // Devolver las reservas
-      }
+        if (err) {
+            console.error('Error al obtener las reservas:', err);
+            res.status(500).json({ message: 'Error al obtener las reservas' });
+        } else {
+            console.log("Reservas encontradas:", result); // Verifica qué datos está devolviendo la base de datos
+            res.status(200).json(result); // Devolver las reservas
+        }
     });
-  };
-  
+};
