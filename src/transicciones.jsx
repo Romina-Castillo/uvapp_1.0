3751dc8d51dd9dd817a4eef077cca9efe681de14
abriilaballay <abriilaballay@gmail.com>
2024-10-14 19:27:38 -0300

    cambios de body

--- src/transicciones.jsx
+++ src/transicciones.jsx
@@ -5,11 +5,11 @@ import { motion } from 'framer-motion';
 const Transicciones = ({ children }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20, rotate: 0 }} // Movimiento hacia arriba y ligera rotación
-      animate={{ opacity: 1, y: 0, scale: 1, rotate: 0 }} // Aparecer sin escalado
-      exit={{ opacity: 0, y: 20, scale: 0.95, rotate: -2 }} // Salir suavemente
-      transition={{ duration: 0.5, ease: "easeInOut" }} // Duración más sutil
-      style={{ width: '100%', height: '100%', borderRadius: '10px', padding: '20px' }} // Fondo blanco más transparente
+      initial={{ opacity: 0, y: 20, rotate: 0 }} 
+      animate={{ opacity: 1, y: 0, scale: 1, rotate: 0 }} 
+      exit={{ opacity: 0, y: 20, scale: 0.95, rotate: -2 }} 
+      transition={{ duration: 0.5, ease: "easeInOut" }} 
+      style={{ width: '100%', height: '100%', borderRadius: '10px', padding: '20px' }} 
     >
       {children}
     </motion.div>
