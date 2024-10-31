c625cd97b2b06b5901526d53cc8a4b2c911b52d4
Romina-Castillo <rominacastillo054@gmail.com>
2024-10-31 17:55:40 -0300

    ver cambios

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,30 +7,43 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import NavListDrawer from "./NavListDrawer";
 import MenuIcon from '@mui/icons-material/Menu';
+
+// Importa los datos de bodegas, eventos, industrias y programas
 import { bodegasData } from '../../views/Bodegas.jsx';
+import { eventosData } from '../../views/Eventos.jsx';
+import { industriasData } from '../../views/Industrias.jsx';
+import { programs } from '../../views/OfertaEdu.jsx';
 
 export default function Navbar({ navArrayLinks }) {
     const [open, setOpen] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
-    const [filteredBodegas, setFilteredBodegas] = useState(bodegasData);
+    const [filteredResults, setFilteredResults] = useState([]);
     const [showResults, setShowResults] = useState(false);
     const [notificationsAnchorEl, setNotificationsAnchorEl] = useState(null);
     const [userAnchorEl, setUserAnchorEl] = useState(null);
-    const [filterAnchorEl, setFilterAnchorEl] = useState(null); // Estado para el menú de filtros
+    const [filterAnchorEl, setFilterAnchorEl] = useState(null);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState("");
-    const searchBoxRef = useRef(null); // Ref para el Box de búsqueda
-    const [searchBoxPosition, setSearchBoxPosition] = useState(null); // Posición del Box
+    const searchBoxRef = useRef(null);
+    const [searchBoxPosition, setSearchBoxPosition] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
-        console.log(bodegasData);
-
         const storedUsername = localStorage.getItem("username");
         if (storedUsername) {
             setIsLoggedIn(true);
             setUsername(storedUsername);
         }
+    }, []);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (searchBoxRef.current && !searchBoxRef.current.contains(event.target)) {
+                setShowResults(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
     const handleOpenNotifications = (event) => {
@@ -54,40 +67,44 @@ export default function Navbar({ navArrayLinks }) {
     };
 
     const handleSearch = () => {
-        const filteredResults = bodegasData.filter(bodega =>
-            bodega.name.toLowerCase().includes(searchQuery.toLowerCase())
+        // Combina y filtra resultados de bodegas, eventos, industrias y programas
+        const filteredBodegas = bodegasData.filter(bodega =>
+            bodega.name && bodega.name.toLowerCase().includes(searchQuery.toLowerCase()) 
         );
-        setFilteredBodegas(filteredResults);
-        setShowResults(filteredResults.length > 0);
-        
-        // Calcula la posición del Box después de que se actualice
+        const filteredEventos = eventosData.filter(evento =>
+            evento.name && evento.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+        );
+        const filteredIndustrias = industriasData.filter(industria =>
+            industria.name && industria.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+        );
+        const filteredProgramas = programs.filter(programa =>
+            programa.name && programa.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+        );
+    
+        // Combina todos los resultados filtrados
+        const combinedResults = [
+            ...filteredBodegas.map(bodega => ({ ...bodega, route: `/bodegas/${bodega.id}` })), // Asegúrate de que cada bodega tenga un 'id'
+            ...filteredEventos.map(evento => ({ ...evento, route: `/eventos/${evento.id}` })),
+            ...filteredIndustrias.map(industria => ({ ...industria, route: `/industrias/${industria.id}` })),
+            ...filteredProgramas.map(programa => ({ ...programa, route: `/programas/${programa.id}` })),
+        ];
+    
+        setFilteredResults(combinedResults);
+        setShowResults(combinedResults.length > 0);
+    
         if (searchBoxRef.current) {
             const position = searchBoxRef.current.getBoundingClientRect();
             setSearchBoxPosition(position);
         }
     };
+    
 
     const handleFilterClick = (event) => {
-        setFilterAnchorEl(event.currentTarget); // Abre el menú de filtros
+        setFilterAnchorEl(event.currentTarget);
     };
 
     const handleFilterClose = () => {
-        setFilterAnchorEl(null); // Cierra el menú de filtros
-    };
-
-    const handleFilterByBodega = () => {
-        console.log("Filtrar por Bodega");
-        handleFilterClose(); // Cierra el menú después de seleccionar
-    };
-
-    const handleFilterByEvento = () => {
-        console.log("Filtrar por Evento");
-        handleFilterClose(); // Cierra el menú después de seleccionar
-    };
-
-    const handleFilterByPrograma = () => {
-        console.log("Filtrar por Programa");
-        handleFilterClose(); // Cierra el menú después de seleccionar
+        setFilterAnchorEl(null);
     };
 
     const handleLogout = () => {
@@ -142,9 +159,9 @@ export default function Navbar({ navArrayLinks }) {
                     </Button>
 
                     <Menu anchorEl={filterAnchorEl} open={Boolean(filterAnchorEl)} onClose={handleFilterClose}>
-                        <MenuItem onClick={handleFilterByBodega}>Filtrar por Bodega</MenuItem>
-                        <MenuItem onClick={handleFilterByEvento}>Filtrar por Evento</MenuItem>
-                        <MenuItem onClick={handleFilterByPrograma}>Filtrar por Programa</MenuItem>
+                        <MenuItem onClick={() => console.log("Filtrar por Bodega")}>Filtrar por Bodega</MenuItem>
+                        <MenuItem onClick={() => console.log("Filtrar por Evento")}>Filtrar por Evento</MenuItem>
+                        <MenuItem onClick={() => console.log("Filtrar por Programa")}>Filtrar por Programa</MenuItem>
                     </Menu>
 
                     {isLoggedIn && (
@@ -155,43 +172,22 @@ export default function Navbar({ navArrayLinks }) {
                         </IconButton>
                     )}
 
-                    <IconButton
-                        color="inherit"
-                        onClick={handleOpenUserMenu}
-                        sx={{
-                            transition: 'transform 0.3s ease-in-out, filter 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
-                            '&:hover': {
-                                transform: 'scale(1.2)',
-                                filter: 'brightness(1.5)',
-                                boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.9)',
-                            },
-                            '&:active': {
-                                transform: 'scale(1.1)',
-                            },
-                        }}
-                    >
+                    <IconButton color="inherit" onClick={handleOpenUserMenu}>
                         <AccountCircleIcon />
                     </IconButton>
 
                     <Menu anchorEl={userAnchorEl} open={Boolean(userAnchorEl)} onClose={handleCloseUserMenu}>
                         {isLoggedIn ? (
-                            [
-                                <MenuItem key="mis_reservas" onClick={() => navigate("/usuario_reservas")}>
-                                    Mis reservas
-                                </MenuItem>,
-                                <MenuItem key="cerrar_sesion" onClick={handleLogout}>
-                                    Cerrar sesión
-                                </MenuItem>
-                            ]
+                            <>
+                                <MenuItem onClick={() => navigate("/usuario_reservas")}>Mis reservas</MenuItem>
+                                <MenuItem onClick={() => navigate("/usuario")}>Gestionar cuenta</MenuItem>
+                                <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
+                            </>
                         ) : (
-                            [
-                                <MenuItem key="iniciar_sesion" onClick={handleLogin}>
-                                    Iniciar sesión
-                                </MenuItem>,
-                                <MenuItem key="registrarse" onClick={handleRegister}>
-                                    Registrarse
-                                </MenuItem>
-                            ]
+                            <>
+                                <MenuItem onClick={handleLogin}>Iniciar sesión</MenuItem>
+                                <MenuItem onClick={handleRegister}>Registrarse</MenuItem>
+                            </>
                         )}
                     </Menu>
                 </Toolbar>
@@ -202,39 +198,38 @@ export default function Navbar({ navArrayLinks }) {
             </Drawer>
 
             {/* Resultados de búsqueda */}
-{searchQuery && showResults && searchBoxPosition && (
-    <Box
-        sx={{
-            position: "absolute",
-            top: `${searchBoxPosition.bottom + 20}px`, // margen entre la navbar y los resultados
-            left: `${searchBoxPosition.left}px`,
-            width: `580px`, // resultados ancho igual al del box de búsqueda
-            backgroundColor: "white",
-            zIndex: 1000,
-            border: "1px solid rgba(0, 0, 0, 0.12)",
-            borderRadius: "4px",
-            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-        }}
-    >
-        {filteredBodegas.map((bodega, index) => (
-            <Typography
-                key={index}
-                sx={{
-                    padding: "10px", 
-                    cursor: "pointer",
-                    borderBottom: index !== filteredBodegas.length - 1 ? "1px solid rgba(0, 0, 0, 0.12)" : "none"
-                }}
-                onClick={() => {
-                    navigate(`${bodega.route}`);
-                    setShowResults(false);
-                }}
-            >
-                {bodega.name}
-            </Typography>
-        ))}
-    </Box>
-)}
-
+            {searchQuery && showResults && searchBoxPosition && (
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: `${searchBoxPosition.bottom + 20}px`,
+                        left: `${searchBoxPosition.left}px`,
+                        width: `580px`,
+                        backgroundColor: "white",
+                        zIndex: 1000,
+                        border: "1px solid rgba(0, 0, 0, 0.12)",
+                        borderRadius: "4px",
+                        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+                    }}
+                >
+                    {filteredResults.map((result, index) => (
+                        <Typography
+                            key={index}
+                            sx={{
+                                padding: "10px",
+                                cursor: "pointer",
+                                borderBottom: index !== filteredResults.length - 1 ? "1px solid rgba(0, 0, 0, 0.12)" : "none"
+                            }}
+                            onClick={() => {
+                                navigate(`${result.route}`);
+                                setShowResults(false);
+                            }}
+                        >
+                            {result.name}
+                        </Typography>
+                    ))}
+                </Box>
+            )}
         </>
     );
 }
