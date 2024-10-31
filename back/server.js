8f6a75b59a744a0e69efd80a5778fa14200151b8
Romina-Castillo <rominacastillo054@gmail.com>
2024-10-31 17:03:23 -0300

    prueba

--- back/server.js
+++ back/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./database/db')
+const db = require('./database/db');
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.use("/" , require("./usuarios/rutas/rutas") )   // Rutas de Login, Register y Reseras estan en el mismo archivo de rutas
-
+app.use("/" , require("./usuarios/rutas/rutas") )
 app.listen(3001, () => {
     console.log('Server running on port 3001');
 });
